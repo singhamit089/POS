2808c5311bae14ab3ab12750e96aeb667bbd7b91
Amit Singh <singhamit089@gmail.com>
2018-09-16 14:55:14 +0530

    API Testing Done

--- POSTests/APIClientTest.swift
+++ POSTests/APIClientTest.swift
@@ -23,4 +23,47 @@ class APIClientTest: XCTestCase {
     override func tearDown() {
         super.tearDown()
     }
+   
+    func test_get_request_with_URL() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/photos") else {
+            fatalError("URL can't be empty")
+        }
+        
+        apiClient.get(url: url) { (success, response) in
+            // Return data
+        }
+        
+        XCTAssert(session.lastURL == url)
+        
+    }
+    
+    func test_get_resume_called() {
+        
+        let dataTask = MockURLSessionDataTask()
+        session.nextDataTask = dataTask
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/photos") else {
+            fatalError("URL can't be empty")
+        }
+        
+        apiClient.get(url: url) { (success, response) in
+            // Return data
+        }
+        
+        XCTAssert(dataTask.resumeWasCalled)
+    }
+    
+    func test_get_should_return_data() {
+        let expectedData = "{}".data(using: .utf8)
+        
+        session.nextData = expectedData
+        
+        var actualData: Data?
+        apiClient.get(url: URL(string: "https://jsonplaceholder.typicode.com/photos")!) { (data, error) in
+            actualData = data
+        }
+        
+        XCTAssertNotNil(actualData)
+    }
 }
